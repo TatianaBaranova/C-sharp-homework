ce781f9fa0d94f350748f0a8e32a91ae101fe89f
TatianaBaranova <tatiana.barashkina@gmail.com>
2022-10-20 19:16:19 +0300

    added task 16 seminar 2

---
+++ HW-Seminar2-16/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int weekday = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckWeekday(weekday);
+
+if(result)
+{
+    Console.WriteLine("Да, выходной");
+}
+else
+{
+    Console.WriteLine("Нет, рабочий день");
+}
+
+bool CheckWeekday(int number)
+{
+    int number1 = number;
+    int number2 = number;
+    return number1 == 6 | number2 == 7;
+}
