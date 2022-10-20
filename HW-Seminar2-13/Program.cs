c1de76c3f4cf1e3c49b09463f51770f1654b544a
TatianaBaranova <tatiana.barashkina@gmail.com>
2022-10-20 17:33:02 +0300

    added task 13 seminar 2

---
+++ HW-Seminar2-13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int number13 = Convert.ToInt32(Console.ReadLine());
+
+double newNumber = number13 / 100;
+if(newNumber >= 1) 
+{
+    int thirdDigit = ShowThirdDigit(number13);
+    Console.WriteLine(thirdDigit);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+int ShowThirdDigit(int number)
+{
+    while(number >=1000) number /= 10;
+    int newNumber = number % 10;
+    return newNumber;
+}
