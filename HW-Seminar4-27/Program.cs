48d30083363b64774f6fe8ff32f37f90db5c9cdc
TatianaBaranova <tatiana.barashkina@gmail.com>
2022-10-29 22:14:03 +0300

    добавлена задача 27 семинар 4

---
+++ HW-Seminar4-27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+bool isParsed = int.TryParse(Console.ReadLine(), out int a);
+
+if(!isParsed)
+{
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+int sum = SumDigit(a);
+Console.WriteLine(sum);
+
+int SumDigit(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum = sum + number % 10;
+        number /= 10;
+    }
+    return sum;
+}
